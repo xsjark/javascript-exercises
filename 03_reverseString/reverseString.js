ad200c72a340f323bcaa0c3607207e3d1aa6122d
Ridha Watanabe <xsjark@protonmail.com>
2022-08-19 09:02:37 -0500

    finish p3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    var array = string.split('');
+    var reverse_array = array.reverse();
+    var result = reverse_array.join('')
+    return result 
 };
 
 // Do not edit below this line
