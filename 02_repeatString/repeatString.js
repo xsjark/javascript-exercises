c17599bee6c377002dce81025806ec7a7fa3f3d0
Ridha Watanabe <xsjark@protonmail.com>
2022-08-19 08:55:27 -0500

    finish p2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,7 @@
-const repeatString = function() {
-
+const repeatString = (string, repeat) => {
+  if (repeat > 0) return string.repeat(repeat);
+  if (repeat < 0) return "ERROR";
+  else return "";
 };
-
 // Do not edit below this line
 module.exports = repeatString;
