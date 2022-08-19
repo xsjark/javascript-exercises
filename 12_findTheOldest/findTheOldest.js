8a60ad9e612c8e840539e44570d89bbf061d0768
Ridha Watanabe <xsjark@protonmail.com>
2022-08-19 14:07:34 -0500

    finished 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,9 @@
-const findTheOldest = function() {
-
-};
-
+const findTheOldest = (people) => {
+    var year = new Date().getFullYear()
+    var ordered_people = people.sort((a, b) => (
+        ((a.yearOfDeath || year) - a.yearOfBirth) > ((b.yearOfDeath  || year) - b.yearOfBirth )) ? 1 : -1);
+    var oldest = ordered_people.at(-1)
+    return oldest
+}
 // Do not edit below this line
 module.exports = findTheOldest;
