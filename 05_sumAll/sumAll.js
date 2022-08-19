caa922e0411050419fb1412a384fe20dd7bf8136
Ridha Watanabe <xsjark@protonmail.com>
2022-08-19 11:23:41 -0500

    finished p5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,12 @@
-const sumAll = function() {
-
-};
-
+const sumAll = (start, stop) => {
+    if (start >= 0 && stop >= 0 && typeof start === 'number' && typeof stop === 'number') {
+        var array = Array.from({ length: (Math.abs((stop - start) / 1) + 1 )}, (_, i) => start < stop ? (start + (i * 1)) : start - (i * 1));
+        var array_sum = array.reduce((a, b) => a + b, 0);
+        return array_sum
+    } else {
+        return "ERROR"
+    }
+    
+}
 // Do not edit below this line
 module.exports = sumAll;
