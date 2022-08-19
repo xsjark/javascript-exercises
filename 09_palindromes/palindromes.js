24d461f0ab218ab4871642b1cd72cba6832a5607
Ridha Watanabe <xsjark@protonmail.com>
2022-08-19 12:37:19 -0500

    finished p9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = (string) => {
+    var clean_string = string.replace(/[^\w\s]|_/g, "").replace(/\s+/g, "").toLowerCase();
+    var reverse_string = clean_string.split("").reverse().join("")
+    if ( reverse_string == clean_string) {
+        return true
+    } else {
+        return false
+    }
 };
 
 // Do not edit below this line
