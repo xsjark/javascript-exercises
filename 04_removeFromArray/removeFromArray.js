b29ebfbd27d0b73c70895e59fd352592fa799c05
Ridha Watanabe <xsjark@protonmail.com>
2022-08-19 09:28:40 -0500

    finshed p4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,4 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = (array1, ...numstofilter) => array1.filter(item => numstofilter.indexOf(item) < 0)
 
 // Do not edit below this line
 module.exports = removeFromArray;
