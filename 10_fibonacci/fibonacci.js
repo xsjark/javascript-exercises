875127bb0ccecaa27738ddcd5e8664a7c3d60375
Ridha Watanabe <xsjark@protonmail.com>
2022-08-19 13:18:17 -0500

    finished 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = (n) => {
+  var num = parseInt(n);
+  if (num > 0) {
+    var array = [0, 1];
+    var val = 1;
+    for (let i = 0; i < num - 2; i++) {
+      array.push(array[array.length - 2] + array[array.length - 1]);
+      val = array[array.length - 2] + array[array.length - 1];
+    }
+    return val;
+  } else {
+    return "OOPS";
+  }
 };
 
 // Do not edit below this line
